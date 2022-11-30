430ca966a315d2f27a4ea8d22f7e7b821e170be8
Doğukan <68846195+dogukansimsek16@users.noreply.github.com>
2022-11-30 12:45:58 +0300

    Nested Yapi ASP.NET MVC APP
    Nested Yapi ASP.NET MVC APP

---
+++ ASP.NET MVC NESTED YAPI/ASP.NET MVC NESTED YAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ASP.NET_MVC_NESTED_YAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API yapılandırması ve hizmetleri
+
+            // Web API yolları
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ASP.NET MVC NESTED YAPI/ASP.NET MVC NESTED YAPI/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_NESTED_YAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            List<Notes> list = new List<Notes>();
+            using (VeriTabanimEntities dc = new VeriTabanimEntities())
+            {
+                list = dc.Notes.OrderBy(a => a.NoteID).ToList();
+            }
+                return View();
+        }
+    }
+}
